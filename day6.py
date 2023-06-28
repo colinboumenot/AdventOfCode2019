7aa73b54ec730d6bdfa6f6a4bd3d2ac0225396ed
Christopher Boumenot <colin.boumenot@yahoo.com>
2023-06-28 06:45:32 -0700

    Day 6 Part 1 Complete

--- day6.py
+++ day6.py
@@ -1,1 +1,10 @@
-text = open('inputtxt/day6input.txt').read()
+text = open('inputtxt/day6input.txt').read().splitlines()
+import networkx as nx
+
+graph = nx.DiGraph()
+
+for line in text:
+    graph.add_edge(*[x.strip() for x in line.split(')')])
+
+print(nx.transitive_closure(graph).size())
+print(nx.shortest_path_length(graph.to_undirected(), 'YOU', 'SAN') - 2)
