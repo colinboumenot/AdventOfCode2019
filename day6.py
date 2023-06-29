2ecedaa56ec70efecd6f64007c73e8ccc5dd8935
Christopher Boumenot <colin.boumenot@yahoo.com>
2023-06-29 06:19:32 -0700

    Day 6 Complete

--- day6.py
+++ day6.py
@@ -7,4 +7,5 @@ for line in text:
     graph.add_edge(*[x.strip() for x in line.split(')')])
 
 print(nx.transitive_closure(graph).size())
+
 print(nx.shortest_path_length(graph.to_undirected(), 'YOU', 'SAN') - 2)
