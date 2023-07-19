fc018d2efa42e87d5642af438e07ccf3ad299b5a
Christopher Boumenot <colin.boumenot@yahoo.com>
2023-07-19 12:44:15 -0700

    Day 8 Part 1 Complete

--- day8.py
+++ day8.py
@@ -1,1 +1,16 @@
-text = open('inputtxt/day8input.txt')
+text = open('inputtxt/day8input.txt').read()
+from math import inf
+picture = list(text)
+picture_grid = picture[:25 * 6]
+low = inf
+answer = 0
+
+while picture:
+    layer = picture[:25 * 6]
+    picture = picture[25 * 6:]
+    zeroes = layer.count('0')
+    if zeroes < low:
+        low = zeroes
+        answer = layer.count('1') * layer.count('2')
+
+print(answer)
